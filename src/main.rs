b677cbd84eedc4d477ad30ca1da43e772e6fc172
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2023-01-09 10:32:14 -0800

    add example dir printed using std::fs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let paths = fs::read_dir("./example").unwrap();
+
+    for path in paths {
+        println!("{}", path.unwrap().path().display())
+    }
 }
